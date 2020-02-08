0474c606a35499607137fc6d395e2cb872d83863
gehowes <gehowes@crimson.ua.edu>
2020-02-08 17:51:41 -0600

    Initial Commit

---
+++ foodtrackerapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  foodtrackerapp
+//
+//  Created by Gwendolyn Howes on 08/02/2020.
+//  Copyright © 2020 Gwendolyn Howes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
